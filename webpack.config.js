0d2419823b675afba87a4d1ee280bd0f46523ea2
teple <stotr2@gmail.com>
2019-12-23 03:08:06 +0900

    eslint, prettier ルール追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,28 @@
-const CopyPlugin = require("copy-webpack-plugin");
+/* eslint-disable no-undef */
+const CopyPlugin = require('copy-webpack-plugin')
 
 /**
  * @type import("webpack").Configuration
  */
+
 module.exports = {
-    mode: process.env.NODE_ENV || "development",
-    devtool: "inline-source-map",
-    entry: {
-        background: `${__dirname}/src/background.ts`,
-        main: `${__dirname}/src/main.ts`,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: "ts-loader",
-                exclude: /node_modules/
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".ts", ".js"]
-    },
-    plugins: [
-        new CopyPlugin([
-            { from: "./public", to: "./" }
-        ]),
+  mode: process.env.NODE_ENV || 'development',
+  devtool: 'inline-source-map',
+  entry: {
+    background: `${__dirname}/src/background.ts`,
+    main: `${__dirname}/src/main.ts`
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
     ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [new CopyPlugin([{ from: './public', to: './' }])]
 }
